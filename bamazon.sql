180c2726edb590fd0f32034ed84aaae1368149e3
Alex DJ <deejxue@gmail.com>
2017-07-20 23:16:36 -0700

    final commit

--- bamazon.sql
+++ bamazon.sql
@@ -3,15 +3,15 @@ CREATE DATABASE bamazon_db
 USE bamazon_db;
 CREATE TABLE products (item_id INTEGER(1) AUTO_INCREMENT NOT NULL, product_name VARCHAR(30) NOT NULL, department_name VARCHAR(30) NOT NULL, price DECIMAL(10,2), stock_quantity INTEGER(10), PRIMARY KEY (item_id)
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (1,"bean bags", "sports equipment", "5.00", 25);
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (2,"football", "sports equipment", "12.00", 25);
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (3,"swim caps", "sports equipment", "8.00", 25);
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (4,"boxing shorts", "sports apparel", "13.00", 20);
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (5,"boxing gloves--large", "sports equipment", "30.00", 12);
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (6,"kick paddles", "sports equipment", "35.00", 10);
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (7,"shoulder pads", "sports equipment", "55.00", 5);
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (8,"cleat shoes", "sports apparel", "45.00", 25);
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (9,"swim trunks--men", "sports apparel", "35.00", 25);
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (10,"swimsuits--women", "sports apparel", "65.00", 25);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (1,"  Bean Bags", "sports equipment", "5.00", 25);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (2,"  Standard Football", "sports equipment", "12.00", 25);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (3,"  Swim Caps", "sports equipment", "8.00", 25);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (4,"  Boxing Shorts", "sports apparel", "13.00", 20);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (5,"  Boxing Gloves--LARGE", "sports equipment", "30.00", 12);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (6,"  Kick Boards", "sports equipment", "35.00", 10);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (7,"  Shoulder Pads", "sports equipment", "55.00", 5);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (8,"  Cleat Shoes", "sports apparel", "45.00", 25);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (9,"  Swim Trunks--MEN", "sports apparel", "35.00", 25);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES (10,"  Swimsuits--WOMEN", "sports apparel", "65.00", 25);
 
 SELECT item_id, product_name, department_name, price FROM products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,61 +1,70 @@
-
+// Require MySQL and prompt NPM packages
 var mysql = require('mysql');
 var prompt = require('prompt');
     
     transaction = [];
 
+// Create a connection to the database
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : '',
-    database : 'Bamazon',
+    database : 'bamazon_db',
     multipleStatements : true
 });
 
+// Call on the connection funciton
 connection.connect();
 
-connection.query('SELECT * FROM Products', function(err, results) {
-    if (err) 
-      console.error("error connecting: "+err.stack);}
-    makeTable();
+connection.query('SELECT * FROM products', function(err, results) {
+    if (err) {
+      console.error("error connecting: "+err.stack)
+      makeTable();
 
-    else if { 
+    } else { 
     
-    for (var i = 0; i < results.length; i++) {
-      console.log('Product ID: ', results[i].ItemID);
-      console.log('Product Name: ', results[i].ProductName);
-      console.log('Price: ', results[i].Price);
+      for (var i = 0; i < results.length; i++) {
+        console.log('Product ID: ', results[i].item_id);
+        console.log('Product Name: ', results[i].product_name);
+        console.log('Price: ', results[i].price);
+        }
+      customerBuy(); 
     }
-    cTransaction(); 
 });
 
-function cTransaction(){
+// Function to inquire what the customer would like to buy from the database choices available
+function customerBuy(){
   var productInfo = {
     properties: {
-      ItemID: {description: ' Chose by typing ID'},
-      Quantity: {description: ' Quantity:Num of item ? '}
+      item_id: {description: ' Chose by typing ID'},
+      stock_quantity: {description: ' Quantity: How many items? '}
     },
   };
     
   prompt.start();
     prompt.get(productInfo, function (err, res){
       var purchase = {
-        ItemID: res.ItemID,
-        Quantity: res.Quantity
+        item_id: res.item_id,
+        stock_quantity: res.stock_quantity
       };
 
     transaction.push(purchase);
-            
-    connection.query('SELECT * FROM Products WHERE ItemID=?', transaction[0].ItemID, function(err, res){
-      if (res[0].StockQuantityQuantity >= transaction[0].Quantity) {      
-        console.log('TOTAL'+ (transaction[0].Quantity*res[0].Price));
-        StockQuantityLeft = res[0].StockQuantityQuantity - transaction[0].Quantity;
-        connection.query('UPDATE Products SET StockQuantityQuantity ='+ StockQuantityLeft + ' WHERE ItemID ='+ transaction[0].ItemID, function(err, res){
-        console.log("Order Confrimed");
+    
+    // Take a look at the table and see if the quantity ordered is less than the quantity on hand and confirm order
+    connection.query('SELECT * FROM products WHERE item_id=?', transaction[0].item_id, function(err, res){
+      if (res[0].stock_quantity >= transaction[0].stock_quantity) {  
+        // Repeat order back customer at console window
+        console.log("Your order is " + res[0].product_name);    
+        console.log("TOTAL = $" + (transaction[0].stock_quantity*res[0].price));
+        stock_quantity_left = res[0].stock_quantity - transaction[0].stock_quantity;
+        connection.query('UPDATE products SET stock_quantity ='+ stock_quantity_left + ' WHERE item_id ='+ transaction[0].item_id, function(err, res){
+        // Give order confirmation message
+        console.log("Your order has been confirmed.  Thank you.");
         connection.end();
       });
+      // If the order exceeds the amount on hand, inform customer it is out of stock
       } else {
-          console.log("We are currently out of stock....we'll be back soon!");
+          console.log("Our apologies but we are currently out of stock in that item....we'll be back soon!");
           connection.end();
       }
     });
